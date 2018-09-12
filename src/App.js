32b2d126e65ec52989c51500ddc52c940995f991
sever <saurabh.sircar@theleanapps.com>
2018-09-12 14:01:14 +0530

    button props and default style add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import './components/TextField'
 import TextField from './components/TextField/TextField';
+import Button from "./components/Button";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Draw your components here
-        <TextField />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                Draw your components here
+                <TextField/>
+                <Button/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
+
+class Button extends Component {
+    render() {
+        const {buttonStyle} = this.props;
+
+        let buttonClasses = ['button', buttonStyle].join(' ')
+        return (
+            <div>
+                <button type="button"
+                        className={buttonClasses}
+                        disabled={this.props.disabled}
+                        onClick={this.props.onClick}>
+                    {this.props.buttonText}
+                </button>
+            </div>
+        );
+    }
+}
+
+Button.defaultProps = {
+    buttonText: 'Click Me!',
+    disabled:false
+}
+
+Button.propTypes = {
+    buttonStyle: PropTypes.string,
+    buttonText: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    disabled: PropTypes.bool
+}
+
+export default Button;
